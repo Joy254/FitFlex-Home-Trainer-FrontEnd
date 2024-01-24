85f9c0675875828d3a923dea3bbb0dc4d45552f7
JOY254 <nyorgijoy@gmail.com>
2024-01-24 03:37:22 -0600

    add functionality searchBar

--- FitFlex-Home-Trainer-FrontEnd/src/components/SearchBar.jsx
+++ FitFlex-Home-Trainer-FrontEnd/src/components/SearchBar.jsx
@@ -1,2 +1,29 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+
+function SearchBar({ onSearch }) {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSearch = (e) => {
+        e.preventDefault(); // Prevent form submission
+        const newSearchTerm = searchTerm;
+        onSearch(newSearchTerm);
+      };
+
+      return (
+        <div className="search-bar">
+          <form onSubmit={handleSearch}>
+            <input
+              type="text"
+              placeholder="Search Exercises of your choice👉"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="search-input"
+            />
+            <button type="submit" className="search-button">
+              Search
+            </button>
+          </form>
+        </div>
+      );
+    }

8525862a995c1efa9fe33888459a67d56a5c70a2
JOY254 <nyorgijoy@gmail.com>
2024-01-24 03:10:34 -0600

    adding navbar

--- FitFlex-Home-Trainer-FrontEnd/src/components/NavBar.jsx
+++ FitFlex-Home-Trainer-FrontEnd/src/components/NavBar.jsx
@@ -1,2 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+    return (
+      <nav className="navbar-list">
+        <ul className="navbar-list">
+          <li className="dropdown">
+            <button className="dropdown-toggle">ABOUT US 👇👇👇👇</button>
+            <ul className="dropdown-menu">
+            <li>
+            <Link to="/">HOME 😊</Link>
+          </li>
+          <li>
+          <Link to="/exercise-daily-routine">EXERCISES ROUTINE😊</Link>
+        </li>
+        <li>
+          <Link to="/add-exercise">ADD EXERCISE FORM 😊</Link>
+        </li>
+        <li>
+          <Link to="/comments">REVIEWS 💙😊</Link>
+        </li>
+      </ul>
+    </li>
+  </ul>
+</nav>
+);
+};

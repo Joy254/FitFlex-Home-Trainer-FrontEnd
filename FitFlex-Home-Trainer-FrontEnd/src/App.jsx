561e81f8c08b50fd6a2d17ce1f2c4ff977d1a922
Markswell <osoromarkswell80@gmail.com>
2024-01-25 05:35:43 +0300

    Resolve Conflicts

--- FitFlex-Home-Trainer-FrontEnd/src/App.jsx
+++ FitFlex-Home-Trainer-FrontEnd/src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
 
 import './App.css'
 import Signup from './components/Signup'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
     <Signup/>
     </>
   )
+}
+
+export default App;
